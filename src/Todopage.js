d8c8c65f45916fb4eb9d6b9b8199fc1fdc58e496
twinklebyeol <131242674+twinklebyeol@users.noreply.github.com>
2023-08-08 18:18:39 +0900

    묘둘.css 알아보기

--- src/Todopage.js
+++ src/Todopage.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import styles from "./Todopage.module.css";
 
 
 function Todopage() {
 
+const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div className="App">
-      <div className="todotitle_wrap">
-      <h1 className="todologo">YumDo!</h1>
+      <div className={styles.todo_container}>
+      <h1 className={styles.todotitle}>YumDo!</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
       </div>
-    </div>
+
+    
   );
 }
-
 export default Todopage;
